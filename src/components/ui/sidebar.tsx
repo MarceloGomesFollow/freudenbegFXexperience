441209429561e50f7f166df4cdf79f09b69e3548
Marcelo Gomes <marcelojg@gmail.com>
2025-11-28 22:09:43 +0000

    side bar color background have to change to grey transparente with glass

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -199,7 +199,7 @@ const Sidebar = React.forwardRef<
           <SheetContent
             data-sidebar="sidebar"
             data-mobile="true"
-            className="w-[--sidebar-width] bg-sidebar p-0 text-sidebar-foreground [&>button]:hidden"
+            className="w-[--sidebar-width] bg-sidebar/80 backdrop-blur-lg p-0 text-sidebar-foreground [&>button]:hidden"
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH_MOBILE,
@@ -249,7 +249,7 @@ const Sidebar = React.forwardRef<
         >
           <div
             data-sidebar="sidebar"
-            className={cn("flex h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow", className)}
+            className={cn("flex h-full w-full flex-col bg-sidebar/80 backdrop-blur-lg border-r border-sidebar-border group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow", className)}
           >
             {children}
           </div>
